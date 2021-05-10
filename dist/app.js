202974391b1028521c6f57a6bb6961b3ec56f6c3
enrico bianco <biancoenrico71@gmail.com>
2021-05-10 17:18:05 +0200

    head

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,48 @@
   \********************/
 /***/ (() => {
 
-
+var app = new Vue({
+  el: '#root',
+  data: {
+    navList: [{
+      name: 'Home',
+      list: {
+        element: ''
+      }
+    }, {
+      name: 'Courses',
+      list: {
+        element: ''
+      }
+    }, {
+      name: 'About Us',
+      list: {
+        element: ''
+      }
+    }, {
+      name: 'News',
+      list: {
+        element: ''
+      }
+    }, {
+      name: 'Pages',
+      list: {
+        element: ''
+      }
+    }, {
+      name: 'Contact',
+      list: {
+        element: ''
+      }
+    }, {
+      name: 'Purchase',
+      list: {
+        element: ''
+      }
+    }]
+  },
+  methods: {}
+});
 
 /***/ }),
 
---
+++ src/app.js
@@ -0,0 +1,54 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+      navList:
+      [
+        {
+            name:'Home',
+            list:{
+                element:'',
+            }
+        },
+        {
+            name:'Courses',
+            list:{
+                element:'',
+            }
+        },
+        {
+            name:'About Us',
+            list:{
+                element:'',
+            }
+        },
+        {
+            name:'News',
+            list:{
+                element:'',
+            }
+        },
+        {
+            name:'Pages',
+            list:{
+                element:'',
+            }
+        },
+        {
+            name:'Contact',
+            list:{
+                element:'',
+            }
+        },
+        {
+            name:'Purchase',
+            list:{
+                element:'',
+            }
+        },
+      ]
+    },
+    methods: {
+
+    },
+  
+})
